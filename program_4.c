67d7faf702f78f823ba99a4a69bc455c1fabdb52
DULAN DHANUJAYA <dananjayadulan23@gmail.com>
2025-12-18 18:30:23 +0530

    Create program_4.c

---
+++ program_4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a=19;
+    float b=3.14;
+    char em='e';
+    char na[]="hello !";
+    printf("%d\n",a);
+      printf("%f\n",b);
+        printf("%c\n",em);
+          printf("%s\n",na);
+
+    return 0;
+}
